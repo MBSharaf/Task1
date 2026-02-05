eee2fc2a8e8159a8ae509d9ca774f7693d4e9049
Sharaf <114002993+MBSharaf@users.noreply.github.com>
2026-02-05 22:25:17 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,32 +1,74 @@
-﻿namespace ConsoleApp1test
+﻿using System;
+
+namespace challange1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //    Console.WriteLine("enter your fav num?");
-            //    int FavNum = Convert.ToInt32(Console.ReadLine());
-            //    Console.WriteLine($"No really!! {FavNum} is my fav num ");
 
-            //int[] SchoolArr = [1, 2, 3];
-            //Console.WriteLine($"your array is : {SchoolArr[0]}");
-            //Console.WriteLine($"your array is : {SchoolArr[1]}");
-            //Console.WriteLine($"your array is : {SchoolArr[2]}");
-            //SchoolArr[0] = 99;
-            //Console.WriteLine($"you changed the first number to {SchoolArr[0]}");
 
-            //int[] = Convert.ToInt32( Console.ReadLine());
+            // islam has a carpet cleaning service 
+            // charges
+            // 25$ per small carpet 
+            // 35$ per Large carpet 
+            //sales tax rate us 6% 
+            //est are valid for 30 days 
 
-            string[] studentarray = ["ahmed", "sara", "Mona"];
-            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
-            keyValuePairs.Add(studentarray[0], "active"); 
-            keyValuePairs.Add(studentarray[1], "non active"); 
-            keyValuePairs.Add(studentarray[2], "active");
-            Console.WriteLine(keyValuePairs[studentarray[0]);
-            Console.WriteLine(keyValuePairs[1]);
-            Console.WriteLine(keyValuePairs[2]);
+            //prompt the user for the numbers of small and large roons they would like cleaned and provide an est such as 
 
-        
+            //est for carpet cleaning service 
+            //number of small carpet is 3
+            //number of large carpet is 1
+            //price for small carpet is 25$ 
+            //price for large carpet is 35$ 
+            //cost = $110
+            //tax = $6.6 
+
+            //total est price is 116.6
+            //this est is valid for 30 days 
+
+
+
+            // main task 
+
+            int smallCarpetPrice = 25;
+            int largeCarpetPrice = 35;
+            double taxRate = 6.6;
+
+            Console.WriteLine("Enter the number of small carpets you would like to clean: ");
+            int smallCarpetNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of large carpets you would like to clean: ");
+            int largeCarpetNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"You Asked to clean {smallCarpetNum} Small Carpet");
+            Console.WriteLine($"You Asked to clean {largeCarpetNum} Large Carpet");
+
+            int smallPrice = Convert.ToInt32(smallCarpetPrice * smallCarpetNum);
+            Console.WriteLine($"The Price for Small carpet is : {smallPrice}$ ");
+
+            int largPrice = Convert.ToInt32(largeCarpetPrice * largeCarpetNum);
+            Console.WriteLine($"The Price for Large carpet is : {largPrice}$ ");
+
+            Console.WriteLine($"The Taxs is : {taxRate}$ ");
+
+            int cost = (smallPrice) + (largPrice);
+            double finalTax = (cost + taxRate);
+
+            Console.WriteLine($"your Total Cost is : {finalTax}$");
+            Console.WriteLine("Please be informed this est is valid for 30 days");
+            Console.WriteLine("Happy to serve you from EreaSoft :)");
+
+
+            // sreach task 
+
+            int a = 5;
+            int s = 10;
+            Console.WriteLine($"Equation: {a} + {s} = {a + s:c} EGP");
+
+
+
 
         }
     }
